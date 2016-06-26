e1f501f91f6b43b027a9fcb3c482fb11b96b28d2
Ashish Jagtap <ashish_jagtap@neovasolutions.com>
2016-06-27 01:47:02 +0530

    Added comments and formated the code

--- src/main/java/com/workscape/model/VehicleType.java
+++ src/main/java/com/workscape/model/VehicleType.java
@@ -1,11 +1,6 @@
 package com.workscape.model;
 
 public enum VehicleType {
-	/*BIG_WHEEL("Big Wheel", new Vehicle(Constants.PLASTIC_FRAME, Constants.HUMAN_POWERTRAIN, Constants.BIG_WHEELS_LIST)),
-	BICYCLE("Bicycle", new Vehicle(Constants.METAL_FRAME, Constants.HUMAN_POWERTRAIN, Constants.BICYCLE_WHEELS_LIST)),
-	MOTORCYCLE("Motorcycle", new Vehicle(Constants.METAL_FRAME, Constants.INT_COM_POWERTRAIN, Constants.MOTORCYCLE_WHEELS_LIST)),
-	HANG_GLIDER("Hang Glider", new Vehicle(Constants.PLASTIC_FRAME, Constants.BERNOULLI_POWERTRAIN, new ArrayList<Wheel>())),
-	CAR("Car", new Vehicle(Constants.PLASTIC_FRAME, Constants.BERNOULLI_POWERTRAIN, Constants.CAR_WHEELS_LIST));*/
 	BIG_WHEEL("Big Wheel"),
 	BICYCLE("Bicycle"),
 	MOTORCYCLE("Motorcycle"),
